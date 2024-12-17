9c1e7a3127f3fc0d13300a6118f38229e57b360b
SamiChafik <sami.chafik00@gmail.com>
2024-12-17 16:19:17 +0100

    added a small menu + first version of two methods

--- Book.java
+++ Book.java
@@ -1,7 +1,11 @@
-import java.util.ArrayList;
-
 public class Book {
     String title;
     String author;
     int ISBN;
+
+    public Book(String title, String author, int ISBN) {
+        this.title = title;
+        this.author = author;
+        this.ISBN = ISBN;
+    }
 }
--- Main.java
+++ Main.java
@@ -1,19 +1,55 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
-        Book obj = new Book();
-        Scanner sc = new Scanner(System.in);
-        //Scanner sc2 = new Scanner(System.in);
-        System.out.print("Entre a book name : ");
-        obj.title = sc.nextLine();
-        System.out.print("Entre a book author : ");
-        obj.author = sc.nextLine();
-        System.out.print("Entre a book's ISBN : ");
-        obj.ISBN = sc.nextInt();
+    static ArrayList<Book> books = new ArrayList<>();
 
-        System.out.println(obj.title);
-        System.out.println(obj.author);
-        System.out.println(obj.ISBN);
+    static void add(){
+        //Book obj = new Book();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Entre a book name : ");
+        String title = sc.nextLine();
+        System.out.print("Entre a book author : ");
+        String author = sc.nextLine();
+        System.out.print("Entre a book's ISBN : ");
+        int ISBN = sc.nextInt();
+        books.add(new Book(title, author, ISBN));
+        System.out.println("Book added successfully!\n");
+    }
+
+    static void show() {
+        if (books.isEmpty()){
+            System.out.println("No books stored.");
+        }
+        for (int i = 0; i < books.size(); i++) {
+            Book book = books.get(i);
+            System.out.println("\n=== list of books ===");
+            System.out.println("book " + (i + 1) + " :");
+            System.out.println("title : " + book.title);
+            System.out.println("author : " + book.author);
+            System.out.println("ISBN : " + book.ISBN);
+        }
+    }
+    public static void main(String[] args) {
+        int ch;
+        do {
+
+            Scanner ch1 = new Scanner(System.in);
+            System.out.print("=== Menu ===\n1-add a book\n2-show a book\n0-exit\n=> enter your choice : ");
+            ch = ch1.nextInt();
+            switch(ch) {
+                case 1 :
+                    add();
+                    break;
+                case 2:
+                    show();
+                    break;
+                case 0:
+                    System.out.println("exiting program...");
+                    break;
+                default:
+                    System.out.println("\nInvalid choice try again..");
+            }
+        } while (ch != 0);
     }
 }
