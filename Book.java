fbaba78efbf1027d132c7a2b4a7b0e953aa5c430
SamiChafik <sami.chafik00@gmail.com>
2024-12-18 14:50:16 +0100

    added more to add/show methods

--- Book.java
+++ Book.java
@@ -2,10 +2,12 @@ public class Book {
     String title;
     String author;
     int ISBN;
+    boolean available;
 
-    public Book(String title, String author, int ISBN) {
+    public Book(String title, String author, int ISBN, boolean available) {
         this.title = title;
         this.author = author;
         this.ISBN = ISBN;
+        this.available = available;
     }
 }
--- Main.java
+++ Main.java
@@ -6,6 +6,7 @@ public class Main {
 
     static void add(){
         //Book obj = new Book();
+        boolean available = true;
         Scanner sc = new Scanner(System.in);
         System.out.print("Entre a book name : ");
         String title = sc.nextLine();
@@ -13,29 +14,83 @@ public class Main {
         String author = sc.nextLine();
         System.out.print("Entre a book's ISBN : ");
         int ISBN = sc.nextInt();
-        books.add(new Book(title, author, ISBN));
+
+        books.add(new Book(title, author, ISBN, available));
         System.out.println("Book added successfully!\n");
     }
 
     static void show() {
-        if (books.isEmpty()){
-            System.out.println("No books stored.");
-        }
-        for (int i = 0; i < books.size(); i++) {
-            Book book = books.get(i);
-            System.out.println("\n=== list of books ===");
-            System.out.println("book " + (i + 1) + " :");
-            System.out.println("title : " + book.title);
-            System.out.println("author : " + book.author);
-            System.out.println("ISBN : " + book.ISBN);
-        }
+        int ch;
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.print("\n1 - show all books\n2 - show a specific book\n0 - Exit\nenter your choice : ");
+            ch = sc.nextInt();
+
+            switch (ch) {
+                case 1:
+                    if (books.isEmpty()) {
+                        System.out.println("No books stored.");
+                        return;
+                    }
+
+                    for (int i = 0; i < books.size(); i++) {
+                        System.out.println("\n=== list of books ===");
+                        Book book = books.get(i);
+
+                        System.out.println("book " + (i + 1) + " :");
+                        System.out.println("title : " + book.title);
+                        System.out.println("author : " + book.author);
+                        System.out.println("ISBN : " + book.ISBN);
+                        if (!book.available) {
+                            System.out.println("this book is available.");
+                        } else System.out.println("this book is not available");
+                    }
+                    break;
+
+                case 2:
+                    if (books.isEmpty()) {
+                        System.out.println("No books stored.");
+                        return;
+                    }
+
+                    Scanner sc2 = new Scanner(System.in);
+                    System.out.println("ISBN : ");
+                    int scISBN = sc2.nextInt();
+                    for (Book book : books) {
+                        if (book.ISBN == scISBN) {
+                            System.out.println("title : " + book.title);
+                            System.out.println("author : " + book.author);
+                            System.out.println("ISBN : " + book.ISBN);
+                            if (book.available) {
+                                System.out.println("this book is available.");
+                            } else System.out.println("this book is not available");
+                            System.out.println("\nwant to change availability ?\n1-yes\n2-no/exit");
+                            ch = sc.nextInt();
+                            if (ch == 1) {
+                                book.available = !book.available; // Toggle availability
+                                System.out.println("Book updated successfully!");
+                            }
+                            break;
+                        }
+                    }
+                    break;
+
+                case 0:
+                    break;
+                default:
+                    System.out.println("invalid choice try again !\n");
+            }
+        }while (ch < 0 || ch > 2);
     }
+
+    static void find_modify(){}
+    static void delete(){}
+
     public static void main(String[] args) {
         int ch;
         do {
-
             Scanner ch1 = new Scanner(System.in);
-            System.out.print("=== Menu ===\n1-add a book\n2-show a book\n0-exit\n=> enter your choice : ");
+            System.out.print("=== Menu ===\n1 - add a book\n2 - show all books\n3 - find/modify a book\n4 - delete a book\n0 - exit\n=> enter your choice : ");
             ch = ch1.nextInt();
             switch(ch) {
                 case 1 :
@@ -44,6 +99,11 @@ public class Main {
                 case 2:
                     show();
                     break;
+                case 3 :
+                    find_modify();
+                    break;
+                case 4 :
+                    delete();
                 case 0:
                     System.out.println("exiting program...");
                     break;
